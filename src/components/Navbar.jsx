355b41ee63e7a37912ef3feb4953955ef380bcde
GRM VISHNU <grmvishnu123@gmail.com>
2025-03-18 00:32:46 -0400

    Made navbar responsive to all screen sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      <div className="w-full flex flex-nowrap justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
           className="flex items-center gap-2"
@@ -27,12 +27,14 @@ const Navbar = () => {
             alt="logo"
             className="w-9 h-9 object-contain"
           />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+
+          <p className="text-white text-[16px] xl:text-[18px] font-bold cursor-pointer flex truncate max-w-xs">
             Guturu Rama Mohan Vishnu
           </p>
         </Link>
 
-        <ul className="list-none hidden sm:flex flex-row gap-10">
+        {/* Desktop Menu: only visible on xl (>=1280px) */}
+        <ul className="list-none hidden xl:flex flex-row gap-10">
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -46,7 +48,8 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        {/* Mobile Menu Toggle: visible on screens below xl */}
+        <div className="xl:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
             alt="menu"
@@ -56,9 +59,9 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl flex-col gap-4`}
           >
-            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+            <ul className="list-none flex flex-col items-center gap-4">
               {navLinks.map((link) => (
                 <li
                   key={link.id}
